97078d9984b6fdcdda99d0f06e0e3b7f86c6ef2f
Matheus.BAlves <matheus.balves@outlook.com>
2025-02-27 20:38:54 -0300

    Renamed files and created README

--- index.php
+++ index.php
@@ -1,43 +1,32 @@
 <?php
-$pastaOrigem = "D:\Alceu\xml";
+$pastaOrigem = "DiretorioDeOrigem";
 $arquivos = glob($pastaOrigem . "/*.xml");
 
-foreach ($arquivos as $arquivo) {
+$arquivoTxt = './convertidas/XMLs.txt';
+file_put_contents($arquivoTxt, '');
 
-  $arquivoTxt = './convertidas/' . pathinfo($arquivo, PATHINFO_FILENAME) . '.txt';
-  file_put_contents($arquivoTxt, '');
+$conteudo = '';
+
+foreach ($arquivos as $arquivo) {
 
   $xml = simplexml_load_file($arquivo);
   $xml->registerXPathNamespace("nfe", "http://www.portalfiscal.inf.br/nfe");
 
-
-  $cnpjEmitente = $xml->xpath('//nfe:emit/nfe:CNPJ')[0];
-  $numNF = $xml->xpath('//nfe:ide/nfe:nNF')[0];
-  $serieNF = $xml->xpath('//nfe:ide/nfe:serie')[0];
-  $dataEmissao = $xml->xpath('//nfe:ide/nfe:dEmi')[0];
-  $dataEmissaoformat = DateTime::createFromFormat('Y-m-d', $dataEmissao)->format('d/m/Y');
-
-
-  $conteudo = '';
-  $conteudo .= "Arquivo: " . basename($arquivo);
-  $conteudo .= "\nCNPJ Emitente: $cnpjEmitente\n";
-  $conteudo .= "Número NF: $numNF\n";
-  $conteudo .= "Série NF: $serieNF\n";
-  $conteudo .= "Data de emissão: $dataEmissaoformat\n\n";
-
-  $contador = 1;
-
   foreach ($xml->xpath('//nfe:det/nfe:prod') as $produto) {
-    $codProduto = $produto->cProd;
-    $descProduto = $produto->xProd;
-    $valorProduto = $produto->vProd;
-    $qtdProduto = $produto->qCom;
-    $conteudo .= "Produto $contador\n";
-    $conteudo .= "Código Produto: $codProduto \n";
-    $conteudo .= "Descrição Produto: $descProduto \n";
-    $conteudo .= "Quantidade Produto: $qtdProduto \n";
-    $conteudo .= "Valor Produto: R$ $valorProduto \n\n";
-    $contador++;
+
+    $cnpjEmitente = $xml->xpath('//nfe:emit/nfe:CNPJ')[0];
+    $numNF = str_pad($xml->xpath('//nfe:ide/nfe:nNF')[0], 10, '0', STR_PAD_LEFT);
+    $serieNF = str_pad($xml->xpath('//nfe:ide/nfe:serie')[0], 3, '0', STR_PAD_LEFT);
+    $dataEmissao = $xml->xpath('//nfe:ide/nfe:dEmi')[0];
+    $dataEmissaoformat = DateTime::createFromFormat('Y-m-d', $dataEmissao)->format('Ymd');
+    $codProduto = str_pad($produto->cProd, 10, '0', STR_PAD_LEFT);
+    $descProduto = str_pad($produto->xProd, 200, ' ', STR_PAD_RIGHT);
+    $valorProduto = (int) $produto->vProd;
+    $partes = explode('.', number_format($valorProduto, 4, '.', ''));
+    $parteInt = str_pad($partes[0], 12, '0', STR_PAD_LEFT);
+    $parteDec = $partes[1];
+
+    $conteudo .= "$cnpjEmitente$numNF$serieNF$codProduto$descProduto$parteInt,$parteDec$dataEmissaoformat\n";
   }
-  file_put_contents($arquivoTxt, $conteudo, FILE_APPEND);
 }
+file_put_contents($arquivoTxt, $conteudo, FILE_APPEND);
